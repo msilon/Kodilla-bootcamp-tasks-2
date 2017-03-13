55356ae208fbf70bf3529c9d2a85394f997ce4bc
msilon <willardan@gmail.com>
2017-03-13 13:03:43 +0100

    Zadanie 10.2

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function Telefon(marka, cena, kolor, poziomSzpanerstwa, rozdzielczosc) {
-	this.marka = marka; 
-	this.cena = cena;
-	this.kolor = kolor;
-	this.poziomSzpanerstwa = poziomSzpanerstwa;
-	this.rozdzielczosc = rozdzielczosc;
-};
+function Button(text) {
+	this.text = text || 'Hello';
+}
 
-Telefon.prototype.printInfo = function() {
-	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ". " + "Poziom szpanerstwa to: " + this.poziomSzpanerstwa + ", a rozdzielczość: " + this.rozdzielczosc + ".");
-};
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
 
-var iPhone6S = new Telefon("Apple", 2250, "srebrny", "10", "1334x750");
-var Samsung_Galaxy_S6 = new Telefon("Samsung", 1500, "turkusowy", "8", "1440x2560");
-var OnePlus_One = new Telefon("OnePlus", 3000, "migdałowy", "5", "1080x1920");
+	}
+}
 
-iPhone6S.printInfo();
-Samsung_Galaxy_S6.printInfo();
-OnePlus_One.printInfo();
+var btn1 = new Button('Hello!');
+btn1.create(); 
+
